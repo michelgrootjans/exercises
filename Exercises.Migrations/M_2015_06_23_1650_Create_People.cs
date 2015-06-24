31f9248728ff4228c1bd6a260aeecdad4ad06236
Michel Grootjans <michel.grootjans@gmail.com>
2015-06-24 14:55:39 +0200

    vanalles

---
+++ Exercises.Migrations/M_2015_06_23_1650_Create_People.cs
@@ -0,0 +1,35 @@
+﻿using FluentMigrator;
+
+namespace Exercises.Migrations
+{
+    [Migration(201506231650)]
+    public class M_2015_06_23_1650_Create_People : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("People").WithDescription("All people in the world").InSchema("dbo")
+                .WithColumn("Id").AsGuid().PrimaryKey();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("People").InSchema("dbo");
+        }
+    }
+
+    [Migration(201506231700)]
+    public class M_2015_06_23_1700_Add_Name_to_People : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("People").WithDescription("All people in the world").InSchema("dbo")
+                .AddColumn("Name").AsString(100).NotNullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Column("Name")
+                .FromTable("People").InSchema("dbo");
+        }
+    }
+}
---
+++ Exercises.Tdd/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercises.Tdd
+{
+    public class Person
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        protected Person() { } // speciaal voor NHibernate
+
+        public Person(string name)
+        {
+            this.name = name;
+        }
+    }
+}
